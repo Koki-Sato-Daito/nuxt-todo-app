a5527670e7a3dee337f18d1eedec06f4e25123bb
koki <s17161278@st.daito.ac.jp>
2022-02-14 10:46:29 +0900

    Github Pagesに公開する設定を追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,13 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  router: {
+    base: '/nuxt-todo-app/'
+  },
+
+  generate: {
+    dir: 'docs'
+  },
 }
